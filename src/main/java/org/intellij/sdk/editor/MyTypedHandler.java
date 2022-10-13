2f0bcf9d852c82c508631576298367ece0e02ea8
Yuriy Lipchey <y.lipchey@do-it.co>
2022-10-13 22:46:38 +0300

    add editor opened support

--- src/main/java/org/intellij/sdk/editor/MyTypedHandler.java
+++ src/main/java/org/intellij/sdk/editor/MyTypedHandler.java
@@ -6,14 +6,27 @@ import com.intellij.codeInsight.editorActions.TypedHandlerDelegate;
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationType;
 import com.intellij.notification.Notifications;
+import com.intellij.openapi.Disposable;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.event.DocumentListener;
 import com.intellij.openapi.editor.event.EditorMouseEvent;
 import com.intellij.openapi.editor.event.EditorMouseListener;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.fileEditor.FileEditorManagerAdapter;
+import com.intellij.openapi.fileEditor.FileEditorManagerEvent;
+import com.intellij.openapi.fileEditor.FileEditorManagerListener;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileManager;
+import com.intellij.openapi.vfs.newvfs.BulkFileListener;
+import com.intellij.openapi.vfs.newvfs.events.VFileEvent;
 import com.intellij.psi.PsiFile;
+import com.intellij.util.messages.MessageBus;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 /**
  * This is a custom {@link TypedHandlerDelegate} that handles actions activated keystrokes in the editor.
@@ -29,11 +42,43 @@ class MyTypedHandler extends TypedHandlerDelegate {
   public Result charTyped(char c, @NotNull Project project, @NotNull Editor editor, @NotNull PsiFile file) {
     // Get the document and project
     final Document document = editor.getDocument();
-    editor.addEditorMouseListener(new CustomListener());
+    @NotNull DocumentListener tess;
+   // editor.addEditorMouseListener(new CustomListener());
+    @NotNull BulkFileListener listener;
+    @NotNull Disposable disposable = null;
+
+    project.getMessageBus().connect().subscribe(VirtualFileManager.VFS_CHANGES,
+            new BulkFileListener() {
+              @Override
+              public void after(@NotNull List<? extends VFileEvent> events) {
+                Notifications.Bus.notify(new Notification("Custom Notification Group","File changed!", NotificationType.INFORMATION));
+
+              }
+            });
     // Construct the runnable to substitute the string at offset 0 in the document
     Runnable runnable = () -> document.insertString(0, "editor_basics\n");
     // Make the document change in the context of a write action.
     WriteCommandAction.runWriteCommandAction(project, runnable);
+
+    MessageBus messageBus = project.getMessageBus();
+    messageBus.connect().subscribe(FileEditorManagerListener.FILE_EDITOR_MANAGER, new FileEditorManagerListener() {
+      @Override
+      public void fileOpened(@NotNull FileEditorManager source, @NotNull VirtualFile file) {
+        Editor editor = source.getSelectedTextEditor();
+        Notifications.Bus.notify(new Notification("Custom Notification Group","File opened!" + file.getName(), NotificationType.INFORMATION));
+
+
+      }
+
+      @Override
+      public void fileClosed(@NotNull FileEditorManager source, @NotNull VirtualFile file) {
+      }
+
+      @Override
+      public void selectionChanged(@NotNull FileEditorManagerEvent event) {
+      }
+    });
+
     return Result.STOP;
   }
 }
