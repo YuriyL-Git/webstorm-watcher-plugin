44d1faed602f7b0e45d1f3e0a87c8bf73bfaa444
Yuriy Lipchey <y.lipchey@do-it.co>
2022-10-23 18:15:47 +0300

    add hotkey action support

--- src/main/java/org/intellij/sdk/editor/MainHandler.java
+++ src/main/java/org/intellij/sdk/editor/MainHandler.java
@@ -1,16 +1,14 @@
 package org.intellij.sdk.editor;
 
-import com.intellij.notification.Notification;
-import com.intellij.notification.NotificationType;
-import com.intellij.notification.Notifications;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 
 class MainHandler {
@@ -24,7 +22,16 @@ class MainHandler {
 
     public void onEditorMouseClick(Editor editor, VirtualFile file) {
       //  Notifications.Bus.notify(new Notification("Custom Notification Group", "Clicked2 " + file.getName(), NotificationType.INFORMATION));
+    }
 
+    public void httpCommand(String command) {
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(
+                        URI.create("http://localhost:3131/" + command))
+                .build();
+        var response = client.sendAsync(request, (HttpResponse.ResponseInfo res) -> {
+            return null;
+        });
     }
 
 
---
+++ src/main/java/org/intellij/sdk/editor/MoveToCssClassAction.java
@@ -0,0 +1,39 @@
+package org.intellij.sdk.editor;
+
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.editor.Editor;
+import icons.SdkIcons;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class MoveToCssClassAction extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        MainHandler handler = new MainHandler();
+        handler.httpCommand("move-to-class");
+    }
+
+
+    @Override
+    public void update(AnActionEvent event) {
+        // Enable/disable depending on whether user is editing
+        Editor editor = event.getData(CommonDataKeys.EDITOR);
+        event.getPresentation().setEnabled(editor != null);
+        // Take this opportunity to set an icon for the group.
+        event.getPresentation().setIcon(SdkIcons.Sdk_default_icon);
+    }
+}
